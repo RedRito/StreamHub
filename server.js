0b80fe09c5c4e0a43608f7dd47f12b25a398a961
Brandon <brandonluong2494@gmail.com>
2024-03-03 03:33:36 -0800

    Created mongoose connection and homepage api

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const path = require("path");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const port = 3000;
 
-const routes = require("./routes/routes");
+var mongoose = require('mongoose');
+var mongoURI = process.env.MONGO_URI;
+console.log(mongoURI);
+mongoose.Promise = global.Promise;
+mongoose.connect(mongoURI)
+
+const routes = require('./routes/routes');
 routes(app);
 
 app.get('/api', (req, res) => {
